b2a82b0caec82774d7f89dd7b98dbfad31de9de3
Esteban Araya <esteban.araya@gmail.com>
2012-12-18 22:48:34 -0700

    Add a few comments

--- hello.c
+++ hello.c
@@ -1,5 +1,8 @@
+/*  C does not have any input, output functions. Thus we impor the
+    standard input output library. */
 #include <stdio.h>
 
+/* Every homie needs a main */
 main()
 {
     printf("Hello World!\n");
