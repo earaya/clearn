63afd33b1f3860c443517eea7f770df46181d2b2
Esteban Araya <esteban.araya@gmail.com>
2012-12-19 20:10:10 -0700

    Delete of moved files. Forgot to commit this.

--- hello.c
+++
@@ -1,9 +0,0 @@
-/*  C does not have any input, output functions. Thus we impor the
-    standard input output library. */
-#include <stdio.h>
-
-/* Every homie needs a main */
-main()
-{
-    printf("Hello World!\n");
-}
